be932e88c3d690fbd031b8f4228628ae5ffbb31d
ibrahimtaskin <taskin.ibrahim1@gmail.com>
2021-03-24 12:18:17 +0300

    Added Crud Operations on api

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -16,21 +16,63 @@ namespace API.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IProductRepository _productRepository;
-        private readonly ILogger<Product> _logger;
+        private readonly ILogger<ProductController> _logger;
 
-        public ProductController(IProductRepository productRepository, ILogger<Product> logger)
+        public ProductController(IProductRepository productRepository, ILogger<ProductController> logger)
         {
             _productRepository = productRepository;
             _logger = logger;
         }
 
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<Product>),(int)HttpStatusCode.OK)] // Geri dönüşte beklediğimiz tip
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)] // Geri dönüşte beklediğimiz tip
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
             var products = await _productRepository.GetProducts();
             _logger.LogInformation($"Getirilen ürünler : {products.ToString()}");
             return Ok(products);
+        }
+
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")] // Custom Name, id must 24 length
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)] // Geri dönüşte beklediğimiz tip
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _productRepository.GetProduct(id);
+
+            if (product == null)
+            {
+                _logger.LogError($"{id}'ye sahip product getirilemedi.");
+                return NotFound();
+            }
+            _logger.LogInformation($"Getirilen ürün : {product.ToString()}");
+            return Ok(product);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)] // Geri dönüşte beklediğimiz tip
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.Create(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)] // Geri dönüşte beklediğimiz tip
+        public async Task<ActionResult<Product>> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _productRepository.Update(product));
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)] // Geri dönüşte beklediğimiz tip
+        public async Task<ActionResult<Product>> DeleteProduct(string id)
+        {
+            return Ok(await _productRepository.Delete(id));
         }
     }
 }
